68f8071d36f397244aa2d868b22d15e3490cfdcf
Andre Igor <andreigormenezes@gmail.com>
2022-02-25 10:53:22 -0300

    confirmando

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+const listaOpcoes = document.querySelector(".secaoDetalhesConta__opcoesTransacoes")
+
+listaOpcoes.addEventListener("click", identificarElemento)
+
+function identificarElemento(event){
+    const li = event.target
+    if (li.tagName == "LI"){
+        const id = li.id
+        const secao1 = document.querySelector(`div[data-id="${id}"]`)
+        removerClasse()
+        secao1.classList.add("show")
+    }
+    
+}
+
+function removerClasse(){
+    const divs = document.querySelectorAll(".secaoTransacao .container div")
+    for (let i = 0; i < divs.length; i ++){
+        console.log(divs[i])
+        divs[i].classList.remove("show")
+    }
+}
